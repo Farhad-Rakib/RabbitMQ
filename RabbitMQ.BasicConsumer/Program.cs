0bccab04396cff6214fe90e54e8c65f09682f271
Farhad Rakib <farhadrakib48@gmail.com>
2023-04-23 12:51:18 +0600

    Rabbit MQ basic Implementation

---
+++ RabbitMQ.BasicConsumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory  = new ConnectionFactory() {HostName="localhost"};
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+
+channel.QueueDeclare(queue: "messagebox",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Recieved new message: {message}");
+};
+
+channel.BasicConsume(queue: "messagebox", autoAck: true, consumer: consumer);
+
+Console.WriteLine("Consuming");
+
+Console.ReadKey();
---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName="localhost"};
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "messagebox",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+var message = "Test Message";
+var body = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish("", "messagebox", null, body);
+Console.WriteLine($"Send Message:{message}");
